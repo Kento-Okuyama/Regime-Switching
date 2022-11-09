b166b0b82e1504c3f7b12265e05401424f5d709e
KentOkuyama <kenoku.ychu.ym421@gmail.com>
2022-11-09 16:09:52 +0100

    gradient

--- EKF.R
+++ EKF.R
@@ -83,11 +83,11 @@ for (i in 1:N){
     
     v[i,t] <- y[i,t] - (Logistic(d[1] + Lmd[1] * eta_[i,t,1] + A[1,] %*% X[i,t,]) * stateProb[i,t] + Logistic(d[2] + Lmd[2] * eta_[i,t,2] + A[2,] %*% X[i,t,]) * (1-stateProb[i,t]))
     
-    EF[i,t,] <- Lmd^2 * P_[i,t,] + rnorm(2, mean=0, sd=1)
+    EF[i,t,] <- Lmd^2 * P_[i,t,] + abs(rnorm(2, mean=0, sd=1))
     
-    f[i,t,] <- (2*pi)**(-1/2) * abs(EF[i,t,])**(-1/2) * exp(-1/2 * v[i,t]**2 * EF[i,t,]**(-1)) 
+    f[i,t,] <- (2*pi)**(-1/2) * EF[i,t,]**(-1/2) * exp(-1/2 * v[i,t]**2 * EF[i,t,]**(-1)) 
     
-    Lf[i,t,] <- -1/2 * (log(2*pi) + abs(EF[i,t,]) + v[i,t]**2 * EF[i,t,]**(-1))  
+    Lf[i,t,] <- -1/2 * (log(2*pi) + EF[i,t,] + v[i,t]**2 * EF[i,t,]**(-1))  
     
     K <- P_[i,t,] * Lmd * EF[i,t,]**(-1)
     
@@ -105,4 +105,75 @@ for (i in 1:N){
       logLL[i,t] <- logLL[i,t-1]
   }
 }
-sum(logLL)
+sumLogLL <- sum(logLL)
+
+# derivatives
+# step 1
+dv <- array(0, c(2,1))
+dF <- array(0, c(2,1))
+
+dd <- array(0, c(2,1))
+dLmd <- array(0, c(2,1))
+dA <- array(0, c(2,nVar))
+
+for (i in 1:N){
+  for (t in 2:Nt){
+    
+    stateProbs <- array(c(stateProb[i,t], 1-stateProb[i,t]), c(2,1))
+    X_it <- array(X[i,t,], c(nVar, 1))
+    
+    dv <- dv - v[i,t] / EF[i,t,] * Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,]) * (1-Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,])) * stateProbs
+    
+    dF <- dF - 1/2 * (EF[i,t,]**(-1) - v[i,t]**2 / EF[i,t,]**2) * stateProbs
+    
+    dd <- dd + dv
+    # dd <- dd - v[i,t] / EF[i,t,] * Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,]) * (1-Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,])) * stateProbs
+    
+    dLmd <- dLmd + dd * eta_[i,t,] + dF * 2 * Lmd * P_[i,t,]
+    # dLmd <- dLmd - v[i,t] / EF[i,t,] * Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,]) * (1-Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,])) * eta_[i,t,] * stateProbs -
+    #   (EF[i,t,]**(-1) - v[i,t]**2 / EF[i,t,]**2) * Lmd * P_[i,t,] * stateProbs
+    
+    dA <- dA + dd %*% t(X_it)
+    # dA <- dA - v[i,t] / EF[i,t,] * Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,]) * (1-Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,])) * stateProbs %*% t(X_it)
+  }
+}
+
+lr <- 0.01
+d <- d + lr * sign(dd)
+Lmd <- Lmd + lr * sign(dLmd)
+A <- A + lr * sign(dA)
+
+dv <- array(0, c(2,1))
+dF <- array(0, c(2,1))
+
+# step 2
+dalpha <- array(0, c(2,1))
+dbeta <- array(0, c(2,1))
+dgamma <- array(0, c(2,nVar))
+
+for (i in 1:N){
+  for (t in 2:Nt){
+    
+    stateProbs <- array(c(stateProb[i,t], 1-stateProb[i,t]), c(2,1))
+    X_it <- array(X[i,t,], c(nVar, 1))
+
+    dv <- dv - v[i,t] / EF[i,t,] * Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,]) * (1-Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,])) * stateProbs
+    
+    dF <- dF - 1/2 * (EF[i,t,]**(-1) - v[i,t]**2 / EF[i,t,]**2) * stateProbs
+    
+    dalpha <- dalpha + dd * Lmd
+    # dalpha <- dalpha - v[i,t] / EF[i,t,] * Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,]) * (1-Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,])) * Lmd * stateProbs
+    
+    dbeta <- dbeta + dalpha * eta[i,t-1,] + dF * 2 * Lmd**2 * beta * P[i,t-1,] 
+    # dbeta <- dbeta - [i,t] / EF[i,t,] * Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,]) * (1-Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,])) * eta[i,t-1,] * stateProbs -
+    #   (EF[i,t,]**(-1) - v[i,t]**2 / EF[i,t,]**2) * Lmd**2 * beta * P[i,t-1,] * stateProbs
+    
+    dgamma <- dgamma + dalpha %*% t(X_it)
+    # dgamma <- dgamma - v[i,t] / EF[i,t,] * Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,]) * (1-Logistic(d + Lmd * eta_[i,t,] + A %*% X[i,t,])) * Lmd * stateProbs %*% t(X_it)
+  }
+}
+
+lr <- 0.01
+alpha <- alpha + lr * sign(dalpha)
+beta <- beta + lr * sign(dbeta)
+gamma <- gamma + lr * sign(dgamma)
